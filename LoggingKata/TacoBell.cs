64fcc46165ff6400ffb30c40330cb582448fe8aa
Antonio Figueroa <antonio_figueroa@dell.com>
2024-10-22 14:04:16 -0500

    - completes taco parser

---
+++ LoggingKata/TacoBell.cs
@@ -0,0 +1,14 @@
+﻿namespace LoggingKata
+{
+    public class TacoBell : ITrackable
+    {
+        public string Name { get; set; }
+        public Point Location { get; set; }
+
+        public TacoBell(string name, Point location)
+        {
+            this.Name = name;
+            this.Location = location;
+        }
+    }
+}
