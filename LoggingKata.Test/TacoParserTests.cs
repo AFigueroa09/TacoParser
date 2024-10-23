e00bf705cf680abab36064850772487271a10256
Antonio Figueroa <antonio_figueroa@dell.com>
2024-10-23 14:21:24 -0500

    - completes taco parsers todos - adds test scenarios

--- LoggingKata.Test/TacoParserTests.cs
+++ LoggingKata.Test/TacoParserTests.cs
@@ -20,6 +20,7 @@ namespace LoggingKata.Test
 
         [Theory]
         [InlineData("34.073638, -84.677017, Taco Bell Acwort...", -84.677017)]
+        [InlineData("34.073638, 0, Taco Bell Acwort...", 0)]
         //Add additional inline data. Refer to your CSV file.
         public void ShouldParseLongitude(string line, double expected)
         {
@@ -35,6 +36,7 @@ namespace LoggingKata.Test
 
         [Theory]
         [InlineData("34.073638, -84.677017, Taco Bell Acwort...", 34.073638)]
+        [InlineData("0, -84.677017, Taco Bell Acwort...", 0)]
         //Add additional inline data. Refer to your CSV file.
         public void ShouldParseLatitude(string line, double expected)
         {
